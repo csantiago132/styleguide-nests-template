847bae7c392d1af2429f0a7c8356ab21eeda67d9
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-02-13 21:26:45 -0600

    chore(config): update husky hooks

--- test/app.controller.test.js
+++ test/app.controller.test.js
@@ -1,19 +1,21 @@
 import { Test } from '@nestjs/testing';
+
 import { AppController } from '../src/app.controller';
 import { AppService } from '../src/app.service';
+
 describe('test AppController', () => {
-    let appController;
-    beforeEach(async () => {
-        const app = await Test.createTestingModule({
-            controllers: [AppController],
-            providers: [AppService],
-        }).compile();
-        appController = app.get(AppController);
+  let appController;
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+    appController = app.get(AppController);
+  });
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
-    describe('root', () => {
-        it('should return "Hello World!"', () => {
-            expect(appController.getHello()).toBe('Hello World!');
-        });
-    });
+  });
 });
 //# sourceMappingURL=app.controller.test.js.map
